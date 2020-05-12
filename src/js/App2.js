324c49e42ec6de74ffc558c2cda25d66f45a23f6
Kate <evgen.knyzhenko@gmail.com>
2020-05-12 16:02:01 +0300

    uglyfly

--- src/js/App2.js
+++ src/js/App2.js
@@ -6,6 +6,7 @@ function App2() {
     <div className="App2">
 <h1>Привіт</h1>
         <article>Я Катерина Нікітченко</article>
+        <img src='https://www.wittemolen.com/sites/default/files/styles/full_width/public/slides/SLIDER-Tropischevogel3.jpg?itok=VFHdHDSn'></img>
     </div>
   );
 }
