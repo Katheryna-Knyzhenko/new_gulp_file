443b1865e8f76b0505f59ac678881034fd1cae09
Kate <evgen.knyzhenko@gmail.com>
2020-05-18 16:01:32 +0300

    mixin

--- src/js/App.js
+++ src/js/App.js
@@ -6,7 +6,15 @@ function App() {
     <div className="App">
 <h1>Привет</h1>
         <article className="block first">Я Катерина Книженко</article>
-        <article className="block">Я Катерина Книженко</article>
+        <article className="block" id= 'central-block'>
+        <ul>
+            <li>1</li>
+            <li>2</li>
+            <li>3</li>
+            <li>4
+                {/* eslint-disable-next-line jsx-a11y/anchor-is-valid */}
+            <a> Ссылка</a></li>
+        </ul></article>
         <article className="block">Я Катерина Книженко</article>
     </div>
   );
